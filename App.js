8a524e01413531920c76e8ed7af9fe4f65827fec
shubhamsc231 <shubhamchopra.5791@gmail.com>
2023-03-09 02:07:01 +0530

    fix

--- App.js
+++ App.js
@@ -162,18 +162,16 @@ const restaurantList = [
 // };
 const Header = () => {
   return (
-    <>
-      <div className="header">
-        <img alt="logo" src={logo} />
-        <h1>Food Walk</h1>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Help</li>
-          <li>Cart</li>
-        </ul>
-      </div>
-    </>
+    <div className="header">
+      <img alt="logo" src={logo} />
+      <h1>Food Walk</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Help</li>
+        <li>Cart</li>
+      </ul>
+    </div>
   );
 };
 const Body = () => {
