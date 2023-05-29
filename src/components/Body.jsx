d2f3a3f9143cc8976bdab6ec878b5327f87a87b3
Shubham Chopra <46850914+shubhamsc231@users.noreply.github.com>
2023-05-29 16:00:42 +0530

    fix

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -12,32 +12,30 @@ const Body = () => {
   const [restaurants, setRestaurants] = useState(restaurantList);
 
   return (
-    <>
-      <div className="search-container">
-        <input
-          type="text"
-          placeholder="Search"
-          className="search-input"
-          value={searchInput}
-          onChange={(e) => {
-            setSearchInput(e.target.value);
-          }}
-        />
-        <button
-          onClick={() => {
-            const data = filterData(searchInput, restaurants);
-            setRestaurants(data);
-          }}
-        >
-          Search
-        </button>
-      </div>
+    <div className="search-container">
+      <input
+        type="text"
+        placeholder="Search"
+        className="search-input"
+        value={searchInput}
+        onChange={(e) => {
+          setSearchInput(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          const data = filterData(searchInput, restaurants);
+          setRestaurants(data);
+        }}
+      >
+        Search
+      </button>
       <div className="cards">
         {restaurants.map((restaurant) => {
           return <RestaurantCard {...restaurant} />;
         })}
       </div>
-    </>
+    </div>
   );
 };
 
