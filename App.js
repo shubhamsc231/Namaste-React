1a519aa9b4f1bea4c9df409e9049f3ee7833ff72
Shubham Chopra <shubhamchopra.5791@gmail.com>
2024-12-25 01:54:30 +0530

    Day-1 revision

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Header from "./src/components/Header";
-import Body from "./src/components/Body";
-import Footer from "./src/components/Footer";
+// import Header from "./src/components/Header";
+// import Body from "./src/components/Body";
+// import Footer from "./src/components/Footer";
 
-const Applayout = () => {
-  return <>
-    <Header></Header>
-    <Body></Body>
-    <Footer></Footer>
-  </>
-};
+// const Applayout = () => {
+//   return <>
+//     ss
+//     {/* <Header></Header>
+//     <Body></Body>
+//     <Footer></Footer> */}
+//   </>
+// };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Applayout />);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<Applayout />);
+
+const heading = React.createElement("div", { id: 'heading1' }, React.createElement("h1", { id: "child" }, "Child I am"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
